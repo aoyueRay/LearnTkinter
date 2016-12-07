b0544c8fd2ecc94e535b1f40e5fadf69e6c2b6cd
aoyueRay <273897153@qq.com>
2016-12-07 15:45:23 +0800

    20161207

--- clock.py
+++ clock.py
@@ -1,21 +1,26 @@
 #-*- coding:utf-8 -*-
 
 import Tkinter as tk
-import time,sys
+import time
 
-root = tk.Tk()
-root.title('Clock')
-# root.minsize(500,500)    # 定义最小的窗口
+class show_clock():
 
-current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-l1 = tk.Label(text=current_time)
-l1.pack()
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Clock')
 
-def update_clock():
-    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    l1.config(text=current_time)
-    root.update()
-    l1.after(1000,update_clock)
-l1.after(1000,update_clock)
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        self.label_time = tk.Label(self.root,text=current_time)
+        self.label_time.pack()
 
-root.mainloop()
+    def update_clock(self):
+        c_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        self.label_time.config(text=c_time)
+        self.root.update()
+        self.label_time.after(1000,self.update_clock)
+
+if __name__ == '__main__':
+    print __name__
+    sc = show_clock()
+    sc.update_clock()
+    sc.root.mainloop()
